be185a8d18ab6a9468807bb0112cbe01170f362e
Sabir Bagwan <106504464+sabirbagwan@users.noreply.github.com>
2022-10-26 09:49:45 +0530

    Update FileNameEdit.py

--- FileNameEdit.py
+++ FileNameEdit.py
@@ -1,10 +1,20 @@
 from os import rename, listdir
 
-badprefix = "GFDLNFO_"
-fnames = listdir( r'E:\1. SEP18')
+badprefix = "bad_"
+fnames = listdir( r'E:\SEP')
 
 for fname in fnames:
     if fname.startswith(badprefix):
         rename(fname, fname.replace(badprefix, '', 1))
+        
+        
+###########################
+
+import os
+for dpath, dnames, fnames in os.walk("D:\\All\\format1\\exp"):
+    for f in fnames:
+        os.chdir(dpath)
+        if f.startswith("badword_"):
+            os.rename(f, f.replace("badword_", ''))
 
         
